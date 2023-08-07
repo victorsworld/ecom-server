146b0fed906d78b8c36fd65c894a0a081db24596
victorsworld <victordirect48@gmail.com>
2023-08-07 16:14:43 -0400

    find one product completed

--- controller/shirtController.js
+++ controller/shirtController.js
@@ -1,4 +1,5 @@
 const Shirt = require('../model/Shirt');
+const User = require('../model/User')
 
 const adminQuantity = async (req, res) => {
   try {
@@ -7,12 +8,10 @@ const adminQuantity = async (req, res) => {
     const existingShirt = await Shirt.findOne({ name });
 
     if (existingShirt) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: 'Product with this name already exists',
-        });
+      return res.status(400).json({
+        success: false,
+        message: 'Product with this name already exists',
+      });
     }
 
     const inventoryInfo = {
@@ -33,9 +32,29 @@ const adminQuantity = async (req, res) => {
   }
 };
 
-const allInventory = async (req, res) => {
-  const findProduct = await Shirt.findOne;
+const oneProduct = async (req, res) => {
+  try {
+    const { _id, name } = req.body;
+    const foundProduct = await Shirt.findOne({ name });
+    if (!foundProduct) {
+      res.status(401).json({ success: false, message: 'Product not found' });
+    }
+    res.status(200).json({ success: true, data: foundProduct });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({success: false, message: error.message})
+  }
 };
+
+const allProduct = async (req, res) =>{
+  try {
+    const user = res.locals.decodedToken.userId;
+    const allProduct = await Todo.find({ owner: user });
+    res.status(200).json({ success: true, data: allTodos });
+  } catch (error) {
+    
+  }
+}
 // get all
 // edit
 // delete
@@ -43,4 +62,4 @@ const allInventory = async (req, res) => {
 //Im going to want to make a request to the user server to make a get request for on shirt and to take it out the inventory 'Get'
 // get shirt by id
 
-module.exports = { adminQuantity };
+module.exports = { adminQuantity, oneProduct };
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {adminQuantity} = require("../controller/shirtController")
+const {adminQuantity, oneProduct, } = require("../controller/shirtController")
 
 router.post('/inventory', adminQuantity)
+
+router.get('/product', oneProduct)
+
+router.get('/all-product',)
 
 module.exports = router;
